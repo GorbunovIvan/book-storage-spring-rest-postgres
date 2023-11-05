043ff2d60266621b6c3e2b7205e986e252f1974c
GorbunovIvan <gorbunovvivan@gmail.com>
2023-11-05 13:35:16 +0200

    swagger is added

--- src/main/java/com/example/BookStorageSpringRestPostgresApplication.java
+++ src/main/java/com/example/BookStorageSpringRestPostgresApplication.java
@@ -3,6 +3,9 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Swagger is located at the link - http://localhost:8080/swagger-ui/index.html
+ */
 @SpringBootApplication
 public class BookStorageSpringRestPostgresApplication {
 
